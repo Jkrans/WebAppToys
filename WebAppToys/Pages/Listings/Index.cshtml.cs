f3ccd2121a1c93580ee00250771aaab77372cb1b
Jeremy Krans <49561917+Jkrans@users.noreply.github.com>
2023-02-23 22:51:34 -0700

    Access to available pages and features setup for 'guest', user, and admin

--- WebAppToys/Pages/Listings/Index.cshtml.cs
+++ WebAppToys/Pages/Listings/Index.cshtml.cs
@@ -1,13 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace WebAppToys.Pages.Listings
 {
-	public class IndexModel : PageModel
+    public class IndexModel : PageModel
     {
         public void OnGet()
         {
---
+++ WebAppToys/wwwroot/js/ListingView.js
@@ -0,0 +1,26 @@
+﻿
+var dataTable;
+
+$(document).ready(function () {
+    loadList();
+});
+
+function loadList() {
+    dataTable = $('#DT_load').DataTable({
+        "ajax": {
+            "url": "/api/listing",
+            "type": "GET",
+            "datatype": "json"
+        },
+        "columns": [
+            { data: "name", width: "40%" },
+            { data: "description", width: "60%" }
+        ],
+        "language": {
+            "emptyTable": "no data found."
+        },
+        "width": "100%"
+    });
+};
+
+
