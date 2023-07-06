21bb35c2242e8fd621f3e8369a17b95affded0c7
Jeremy Krans <49561917+Jkrans@users.noreply.github.com>
2023-07-06 09:24:47 -0600

    Update AuthSenderOptions.cs
    Removed hard coded API key for Sendgrid

--- Infrastructure/Services/AuthSenderOptions.cs
+++ Infrastructure/Services/AuthSenderOptions.cs
@@ -5,7 +5,10 @@ namespace Infrastructure.Services
     {
         private readonly string user = "Toy Swap Hub"; // The name you want to show up on your email
                                                         // Make sure the string passed in below matches your API Key
-        private readonly string key = "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";
+        public AuthSenderOptions()
+        {
+            key = Environment.GetEnvironmentVariable("SENDGRID_API_KEY");
+        }
         public string SendGridUser { get { return user; } }
         public string SendGridKey { get { return key; } }
 
